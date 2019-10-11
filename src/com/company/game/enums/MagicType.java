e34e27a8e75f0fe869cc13d654a067b264032a18
Martin <waldemarsson@gmail.com>
2019-10-11 16:39:56 +0200

    TEST MagicCardFactory buildCard

--- src/com/company/game/enums/MagicType.java
+++ src/com/company/game/enums/MagicType.java
@@ -1,5 +1,21 @@
 package com.company.game.enums;
 
 public enum MagicType {
-    STUN, HEAL_CARD, HEAL_PLAYER, ATTACK_CARD, ATTACK_PLAYER, REMOVE_BUFF, REMOVE_DEBUFF
+    STUN("STUN"),
+    HEAL_CARD("HEALING"),
+    HEAL_PLAYER("SPA_TREATMENT"),
+    ATTACK_CARD("FIREBALL"),
+    ATTACK_PLAYER("ADULT_BULLYING"),
+    REMOVE_BUFF("CRITIQUE"),
+    REMOVE_DEBUFF("CLEANSE");
+
+    final String label;
+
+    MagicType(String label) {
+        this.label = label;
+    }
+
+    public String getLabel(){
+        return this.label;
+    }
 }
--- src/com/company/game/factories/MagicCardFactory.java
+++ src/com/company/game/factories/MagicCardFactory.java
@@ -2,19 +2,17 @@ package com.company.game.factories;
 
 import com.company.game.cards.BuffCard;
 import com.company.game.cards.MagicCard;
+import com.company.game.enums.MagicType;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MagicCardFactory {
     // STUN, HEAL_CARD, HEAL_PLAYER, ATTACK_CARD, ATTACK_PLAYER, REMOVE_BUFF, REMOVE_DEBUFF
 
-
-    private List<String> magicCardNames;
-
     public MagicCardFactory() {
-        magicCardNames = new ArrayList<>(Arrays.asList("Hunter", "Frog", "Donkey", "Knight", "Car", "Ninja", "Dennis", "Ant", "Dragon"));
     }
 
     public MagicCard buildCard(int id) {
@@ -26,14 +24,13 @@ public class MagicCardFactory {
      * @return 3 - 5
      */
     private int getRandomValue() {
-        return 0;
+        return ThreadLocalRandom.current().nextInt(3, 6);
     }
 
-    private String getRandomMonsterName(){
-        return null;
+
+    public MagicType getRandomMagicType() {
+        List<MagicType> types = Arrays.asList(MagicType.ATTACK_CARD, MagicType.ATTACK_PLAYER, MagicType.HEAL_CARD, MagicType.HEAL_PLAYER, MagicType.REMOVE_BUFF, MagicType.REMOVE_DEBUFF, MagicType.STUN);
+        return types.get(ThreadLocalRandom.current().nextInt(0, types.size()));
     }
 
-    private BuffCard getRandomBonus() {
-        return null;
-    }
 }
