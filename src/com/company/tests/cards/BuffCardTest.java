58e199844e65dff190b06f28e0b6c8eb9560efe9
Kristoffer Katra <kristofferkatra@gmail.com>
2019-10-11 14:39:25 +0200

    testing if value is printed in tostring

--- src/com/company/tests/cards/BuffCardTest.java
+++ src/com/company/tests/cards/BuffCardTest.java
@@ -1,5 +1,15 @@
+package com.company.tests.cards;
+
+import com.company.game.cards.BuffCard;
+import com.company.game.enums.EffectType;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuffCardTest {
 
+    @Test
+    void testToString() {
+        assertEquals("DEFENSE_10: +3", new BuffCard(10, 3, EffectType.DEFENSE));
+    }
 }
