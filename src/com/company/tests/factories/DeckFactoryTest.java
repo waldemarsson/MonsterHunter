a878594f5f4eec552ee259d2ee551a5df2345142
Martin <waldemarsson@gmail.com>
2019-10-13 15:30:54 +0200

    TEST deckFactory

--- src/com/company/tests/factories/DeckFactoryTest.java
+++ src/com/company/tests/factories/DeckFactoryTest.java
@@ -1,10 +1,14 @@
 package com.company.tests.factories;
 
+import com.company.game.cards.MonsterCard;
+import com.company.game.collections.Deck;
 import com.company.game.factories.DeckFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,16 +36,44 @@ class DeckFactoryTest {
 
     @Test
     void buildDeck() {
+        Deck deck = deckFactory.buildDeck(10, 10, 10, 10, 10);
+        assertNotNull(deck);
+    }
 
+    @Test
+    void buildDeckNegValue() {
+        Deck deck = deckFactory.buildDeck(-10, 10, 10, 10, 10);
+        assertNotNull(deck);
     }
 
     @Test
     void deckIsRightSize() {
+        for (int i = 0; i < 50; i++) {
+            int numMonster = ThreadLocalRandom.current().nextInt(5, 15);
+            int numEpics = ThreadLocalRandom.current().nextInt(5, 15);
+            int numMagics = ThreadLocalRandom.current().nextInt(5, 15);
+            int numBuffs = ThreadLocalRandom.current().nextInt(5, 15);
+            int numDebuffs = ThreadLocalRandom.current().nextInt(5, 15);
+            int total = numMonster + numEpics + numMagics + numBuffs + numDebuffs;
 
+            Deck deck = deckFactory.buildDeck(numMonster, numEpics, numMagics, numBuffs, numDebuffs);
+            assertEquals(total, deck.cardsLeft());
+        }
     }
 
     @Test
     void deckIsRightAmountOfMonsters() {
+        for (int i = 0; i < 50; i++) {
+            int numMonster = ThreadLocalRandom.current().nextInt(5, 50);
+
+            Deck deck = deckFactory.buildDeck(numMonster, 10, 10 ,10 ,10);
+            deck.getCards().stream().forEach(card -> {
+                System.out.println(card.getClass());
+                System.out.println(MonsterCard.class);
+            });
+            assertEquals(numMonster, deck.getCards().stream().filter(card -> card.getClass() == MonsterCard.class));
+        }
+
 
     }
 
