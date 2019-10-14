2536a0a2a2f1f87c634d92b53f48387b7ab434c6
Martin <waldemarsson@gmail.com>
2019-10-14 13:21:57 +0200

    New File gameTest

---
+++ src/com/company/tests/GameTest.java
@@ -0,0 +1,17 @@
+package com.company.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getGameCLI() {
+    }
+}
