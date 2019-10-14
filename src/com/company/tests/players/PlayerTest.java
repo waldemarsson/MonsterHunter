57043012f372fd4de97b2294efb5c4ee290f805b
Kristoffer Katra <kristofferkatra@gmail.com>
2019-10-14 13:47:13 +0200

    test player hp and damage

--- src/com/company/tests/players/PlayerTest.java
+++ src/com/company/tests/players/PlayerTest.java
@@ -9,10 +9,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    Deck deck = new DeckFactory().buildDeck(10, 10, 10, 10, 10);
+
     @Test
     void constructorTest(){
         try{
-            new Player("Player_1", new DeckFactory().buildDeck(10, 10, 10, 10, 10));
+            new Player("Player_1", deck);
         } catch (Exception e){
             fail();
         }
@@ -21,13 +23,19 @@ class PlayerTest {
     @Test
     void playerHasValidNameUpperCase(){
         assertEquals("PLAYER_1",
-                new Player("Player1", new DeckFactory()
-                        .buildDeck(10, 10, 10, 10, 10)).getName());
+                new Player("Player1", deck);
     }
 
     @Test
-    void playerHasValidHpAndDamage(){
+    void playerHasValidHp(){
+        Player player = new Player("Player_1", deck);
+        assertEquals(20, player.getHp());
+    }
 
+    @Test
+    void playerHasValidDamage(){
+        Player player = new Player("Player_1", deck);
+        assertEquals(0, player.getDamage());
     }
 
     @Test
