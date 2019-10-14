4d651d8a3ec739fc8d47889a6661dfb5368efbbb
Kristoffer Katra <kristofferkatra@gmail.com>
2019-10-14 13:10:43 +0200

    removed tests for getters and setters

--- src/com/company/tests/players/PlayerTest.java
+++ src/com/company/tests/players/PlayerTest.java
@@ -6,29 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
-    @Test
-    void getName() {
-    }
 
-    @Test
-    void getHp() {
-    }
-
-    @Test
-    void getDamage() {
-    }
 
     @Test
     void addDamage() {
     }
 
-    @Test
-    void getHand() {
-    }
 
-    @Test
-    void setBoard() {
-    }
 
     @Test
     void getHandAsString() {
