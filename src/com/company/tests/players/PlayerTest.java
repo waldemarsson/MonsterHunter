b563f9b58c92021d73a9868cb5e0ba51b92c3888
Kristoffer Katra <kristofferkatra@gmail.com>
2019-10-14 13:17:24 +0200

    added empty tests

--- src/com/company/tests/players/PlayerTest.java
+++ src/com/company/tests/players/PlayerTest.java
@@ -1,24 +1,62 @@
 package com.company.tests.players;
 
+import com.company.game.collections.Deck;
+import com.company.game.factories.DeckFactory;
+import com.company.game.players.Player;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    @Test
+    void constructorTest(){
 
+    }
+
+    @Test
+    void playerHasValidNameUpperCase(){
+
+    }
+
+    @Test
+    void playerHasValidHpAndDamage(){
+
+    }
+
+    @Test
+    void playerHasDeckAndHand(){
+
+    }
+
+    @Test
+    void constructorDrawsFiveCardsToHand(){
+
+    }
 
     @Test
     void addDamage() {
+
     }
 
 
+    @Test
+    void drawCardFromDeckToHand(){
+
+    }
+
+    @Test
+    void putCardOnBoardFromHand(){
+
+    }
 
     @Test
     void getHandAsString() {
+
     }
 
     @Test
     void isAlive() {
+
     }
 }
