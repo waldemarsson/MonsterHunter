dc669af5bb45aee3af4964a82c002e0880c48ee9
Kristoffer Katra <kristofferkatra@gmail.com>
2019-10-14 13:02:11 +0200

    init test

--- src/com/company/tests/players/PlayerTest.java
+++ src/com/company/tests/players/PlayerTest.java
@@ -1,5 +1,40 @@
+package com.company.tests.players;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getHp() {
+    }
+
+    @Test
+    void getDamage() {
+    }
+
+    @Test
+    void addDamage() {
+    }
+
+    @Test
+    void getHand() {
+    }
+
+    @Test
+    void setBoard() {
+    }
+
+    @Test
+    void getHandAsString() {
+    }
+
+    @Test
+    void isAlive() {
+    }
 }
